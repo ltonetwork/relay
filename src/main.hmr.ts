2936ed4840e6fa5f852a5b4c6464aca6d8df768d
Moesjarraf J <moesjarraf@gmail.com>
2019-02-06 16:35:40 +0100

    Increase json body-parser limit (#22)
    * Increase json body-parser limit
    
    * Fix event-stream dependency issue

--- src/main.hmr.ts
+++ src/main.hmr.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DispatcherService } from './dispatcher/dispatcher.service';
+import { json } from 'body-parser';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(json({ limit: '5mb' }));
   await app.listen(process.env.PORT || 3000);
 
   const dispatcherService = app.get<DispatcherService>(DispatcherService);
