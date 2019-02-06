f008221791a7aed814e23716099eb2e330a606e5
Moesjarraf J <moesjarraf@gmail.com>
2019-02-06 16:08:04 +0100

    Increase json body-parser limit

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DispatcherService } from './dispatcher/dispatcher.service';
+import { json } from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(json({ limit: '5mb' }));
   await app.listen(process.env.PORT || 80);
 
   const dispatcherService = app.get<DispatcherService>(DispatcherService);
