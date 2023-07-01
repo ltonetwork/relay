f07073a5ee4e621c7251cb1d815bfae4c8a66cf3
arnold <arnold@jasny.net>
2023-06-30 20:41:23 -0400

    Add docker for running rabbitmq. Fixing e2e tests

--- src/common/did-resolver/did-resolver.module.ts
+++ src/common/did-resolver/did-resolver.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DidResolverService } from './did-resolver.service';
+import { RequestModule } from '../request/request.module';
+import { ConfigModule } from '../config/config.module';
 
 @Module({
-  providers: [DidResolverService]
+  imports: [RequestModule, ConfigModule],
+  providers: [DidResolverService],
+  exports: [DidResolverService],
 })
 export class DidResolverModule {}
--- test/queuer.e2e-spec.ts
+++ test/queuer.e2e-spec.ts
@@ -2,38 +2,44 @@ import { Test } from '@nestjs/testing';
 import request from 'supertest';
 import { AppModuleConfig } from '../src/app.module';
 import { INestApplication } from '@nestjs/common';
+import { Account, AccountFactoryED25519, Message } from '@ltonetwork/lto';
+import { ConfigLoaderService } from '../src/common/config/config-loader.service';
+import * as bodyParser from 'body-parser';
 
 describe('Queuer e2e test', () => {
   let app: INestApplication;
+  let config: ConfigLoaderService;
+
+  let sender: Account;
+  let recipient: Account;
+  let message: Message;
 
   beforeAll(async () => {
     const module = await Test.createTestingModule(AppModuleConfig).compile();
-    app = module.createNestApplication();
+    app = module.createNestApplication({ bodyParser: false });
+    app.use(bodyParser.json({ limit: '128mb' }));
+    app.use(bodyParser.raw({ type: 'application/octet-stream', limit: '128mb' }));
+
     await app.init();
+
+    config = module.get<ConfigLoaderService>(ConfigLoaderService);
   });
 
   afterAll(async () => {
     await app.close();
   });
 
+  beforeEach(() => {
+    const factory = new AccountFactoryED25519('T');
+    sender = factory.createFromSeed('sender');
+    recipient = factory.createFromSeed('recipient');
+    message = new Message('hello').to(recipient).signWith(sender);
+  });
+
   describe('POST /queue', () => {
-    test('should add chain to the queue for local node', async () => {
-      const chain = { id: 'fakeid1', events: [{ body: 'fakebody', signkey: 'fakesignkey' }] };
-      const res = await request(app.getHttpServer()).post('/queue').send(chain);
-
+    it('should add chain to the queue for local node', async () => {
+      const res = await request(app.getHttpServer()).post('/').send(message);
       expect(res.status).toBe(204);
-      expect(res.header['content-type']).toBeUndefined();
-      expect(res.body).toEqual({});
-    }, 10000);
-
-    test('should add chain to the queue for remote node', async () => {
-      const chain = { id: 'fakeid2', events: [{ body: 'fakebody', signkey: 'fakesignkey' }] };
-      const to = 'amqp://';
-      const res = await request(app.getHttpServer()).post('/queue').query({ to }).send(chain);
-
-      expect(res.status).toBe(204);
-      expect(res.header['content-type']).toBeUndefined();
-      expect(res.body).toEqual({});
     }, 10000);
   });
 });
