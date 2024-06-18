f76bc5f5dd437dfc10f935032a890806914e34c9
arnold <arnold@jasny.net>
2024-06-18 16:53:41 +0200

    Fix validation of DID document in did resolver. `@context` can be an array

--- src/common/did-resolver/did-resolver.module.ts
+++ src/common/did-resolver/did-resolver.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { DidResolverService } from './did-resolver.service';
 import { RequestModule } from '../request/request.module';
 import { ConfigModule } from '../config/config.module';
+import { LoggerModule } from '../logger/logger.module';
 
 @Module({
-  imports: [RequestModule, ConfigModule],
+  imports: [RequestModule, ConfigModule, LoggerModule],
   providers: [DidResolverService],
   exports: [DidResolverService],
 })
