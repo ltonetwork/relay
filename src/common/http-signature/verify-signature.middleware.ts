6a6d531e47a3f7fb9ad3cbd74b1037c174ab5ae1
otobongfp <otobongfp@gmail.com>
2024-09-27 08:33:43 +0100

    Update: changed how LTO instances are created based on request

--- src/common/http-signature/verify-signature.middleware.ts
+++ src/common/http-signature/verify-signature.middleware.ts
@@ -6,22 +6,13 @@ import { verify } from '@ltonetwork/http-message-signatures';
 export const lto = new LTO();
 @Injectable()
 export class VerifySignatureMiddleware implements NestMiddleware {
-  private lto: LTO;
-
-  constructor() {
-    this.lto = new LTO();
-  }
-
   async verifyRequest(req: Request, res: Response): Promise<boolean> {
     try {
       const path = req.path;
       const walletAddress = path.match(/3[^\/]*/)?.[0];
       const network = getNetwork(walletAddress);
-
-      //switch to testnet if address is testnet
-      if (network == 'T') this.lto = new LTO(network);
-
-      const account = await verify(req, this.lto);
+      const lto = new LTO(network);
+      const account = await verify(req, lto);
       req['signer'] = account;
     } catch (err) {
       res.status(401).json({ message: 'Signature verification failed', error: err.message });
