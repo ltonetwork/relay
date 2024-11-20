1fa21a371bb086ced1d002f33cfe839d0c8a7090
otobongfp <otobongfp@gmail.com>
2024-11-20 01:33:18 +0100

    remove debug route

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,9 @@ import { DispatcherModule } from './dispatcher/dispatcher.module';
 import { QueueModule } from './queue/queue.module';
 import { InboxModule } from './inbox/inbox.module';
 import { VerifySignatureMiddleware } from './common/http-signature/verify-signature.middleware';
-import { DebugModule } from './debug/debug.module';
 
 export const AppModuleConfig = {
-  imports: [ConfigModule, RabbitMQModule, QueueModule, DispatcherModule, InboxModule, DebugModule],
+  imports: [ConfigModule, RabbitMQModule, QueueModule, DispatcherModule, InboxModule],
   controllers: [AppController],
   providers: [AppService],
 };
